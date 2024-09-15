71214359740cb40ec6a2e5bfe0069ddb32c3cc8f
Ghozy Alkhairi <92246826+ghozyalkhairi@users.noreply.github.com>
2024-09-16 00:00:58 +0700

    up

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -2,8 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const CLOUDINARY_API_KEY: string = "";
-export const CLOUDINARY_API_SECRET: string = "";
-export const CLOUDINARY_CLOUD_NAME: string = "";
-export const DATABASE_URL: string = "";
-export const SECRET: string = "";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_SECRET: string =
+  process.env.CLOUDINARY_API_SECRET || "";
+export const CLOUDINARY_CLOUD_NAME: string =
+  process.env.CLOUDINARY_CLOUD_NAME || "";
+export const DATABASE_URL: string = process.env.DATABASE_URL || "";
+export const SECRET: string = process.env.SECRET || "";
