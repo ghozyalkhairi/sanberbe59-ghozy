3ab9d6b0d8acd6b3b134ab865e60fb79e367f1a3
Ghozy Alkhairi <92246826+ghozyalkhairi@users.noreply.github.com>
2024-09-15 23:57:18 +0700

    first commit

---
+++ src/utils/env.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const CLOUDINARY_API_KEY: string = "";
+export const CLOUDINARY_API_SECRET: string = "";
+export const CLOUDINARY_CLOUD_NAME: string = "";
+export const DATABASE_URL: string = "";
+export const SECRET: string = "";
